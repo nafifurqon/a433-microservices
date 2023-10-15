c7f4697942566a087bbbaa9e2f2923c2dfc371a5
Muhammad Nafi' Furqon Diani <muhammadnafi98@gmail.com>
2023-10-15 14:32:04 +0700

    chore: change image repo url

--- build_push_image.sh
+++ build_push_image.sh
@@ -5,10 +5,10 @@ docker build -t item-app:v1 .
 docker images
 
 # tag the image for github packages
-docker tag item-app:v1 docker.pkg.github.com/nafifurqon/item-app/item-app:v1
+docker tag item-app:v1 ghcr.io/nafifurqon/item-app/item-app:v1
 
 # login to github packages
-echo $GITHUB_TOKEN | docker login docker.pkg.github.com -u nafifurqon --password-stdin
+echo $CR_PAT | docker login ghcr.io -u nafifurqon --password-stdin
 
 # push the image to github packages
-docker push docker.pkg.github.com/nafifurqon/item-app/item-app:v1
+docker push ghcr.io/nafifurqon/item-app/item-app:v1
